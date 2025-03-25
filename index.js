ecf73311eecf78abc2a75ec28f5fcd3d62431a58
Samuel Garmany <65299214+samuel-garmany@users.noreply.github.com>
2025-03-24 22:21:21 -0600

    updated index.js with new directory structure

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const axios = require('axios'); // To make HTTP requests from our server. We'll 
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
   extname: 'hbs',
-  layoutsDir: __dirname + '/ProjectSourceCode/views/layouts',
-  partialsDir: __dirname + '/ProjectSourceCode/views/partials',
+  layoutsDir: __dirname + '/src/views/layouts',
+  partialsDir: __dirname + '/src/views/partials',
 });
 
 // database configuration
@@ -52,7 +52,7 @@ db.connect()
 // Register `hbs` as our view engine using its bound `engine()` function.
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'ProjectSourceCode/views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 
 // initialize session variables
@@ -148,7 +148,7 @@ app.use(auth);
 
 app.get('/logout', (req, res) => {
   req.session.destroy(function (err) {
-    res.render('ProjectSourceCode/Pages/logout');
+    res.render('src/Pages/logout');
   });
 });
 
