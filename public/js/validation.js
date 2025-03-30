0cf2190266da5ddc312324650ceb4ae1b182f728
Mason <esslinger.mason@proton.me>
2025-03-30 04:03:27 -0600

    notify user realtime if username is already taken

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,3 +1,4 @@
+// prevent being able to submit if form is invalid
 document.addEventListener('DOMContentLoaded', () => {
   const forms = document.querySelectorAll('.needs-validation');
 
@@ -12,3 +13,51 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.getElementById('register-form')) {
+    const usernameInput = document.getElementById('username');
+
+    usernameInput.addEventListener('input', async () => {
+      await validateUsername(usernameInput);
+    });
+  }
+});
+
+// notify user if username is empty or already taken
+async function validateUsername(usernameInput) {
+  const username = usernameInput.value.trim();
+  const feedback = document.getElementById('invalidUsername');
+
+  if (!username) {
+    setInvalid(usernameInput, feedback, 'Username is required.');
+  } else {
+    try {
+      const res = await fetch('/check-username', {
+        method  : 'POST',
+        headers : { 'Content-Type' : 'application/json' },
+        body    : JSON.stringify({ username })
+      });
+
+      const data = await res.json();
+      if (data.exists) {
+        setInvalid(usernameInput, feedback, 'Username is already taken.');
+      } else {
+        setValid(usernameInput);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+function setInvalid(input, feedback, msg) {
+  input.classList.add('is-invalid');
+  input.classList.remove('is-valid');
+  feedback.textContent = msg;
+}
+
+function setValid(input) {
+  input.classList.add('is-valid');
+  input.classList.remove('is-invalid');
+}
