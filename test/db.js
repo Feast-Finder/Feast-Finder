070b98f422843119363fe5eff7ff998d7f6e56ff
Mason <esslinger.mason@proton.me>
2025-04-07 23:36:31 -0600

    pull database credentials from .env

--- test/db.js
+++ test/db.js
@@ -1,6 +1,13 @@
 // db.js
+
+const conf = {
+  db:       process.env.POSTGRES_DB,
+  user:     process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
 const pgp = require('pg-promise')();
 require('dotenv').config();
-const connectionString =  'postgres://postgres:pwd@db:5432/Feast_Finder_DB';
+const connectionString =  `postgres://${conf.user}:${conf.password}@db:5432/${conf.db}`;
 const db = pgp(connectionString);
 module.exports = db;
