7347abc94801b1984792f5473e04022474d738d9
Mason <esslinger.mason@proton.me>
2025-04-15 14:03:22 -0600

    add minigame selection modal

--- public/js/minigames.js
+++ public/js/minigames.js
@@ -1,88 +1,25 @@
-let currentGroup = null;
+// Should be included in /home
 
-document.getElementById('selectMinigameBtn').addEventListener('click', async (event) => {
-  event.preventDefault(); // Prevent form submission
+document.getElementById('startGameBtn').addEventListener('click', () => {
+  const modal     = bootstrap.Modal.getInstance(document.getElementById('startGameModal'));
+  const form      = document.getElementById('gameForm');
+  const selection = form.querySelector('#gameSelection');
 
-  const minigameForm = document.getElementById('minigameForm');
-  const minigameSelect = minigameForm.querySelector('#minigameSelect');
-  const submitBtn = document.getElementById('selectMinigameBtn');
-
-  const selectedFriends = document.getElementById('minigameSelectedFriends');
-  const userSpans = selectedFriends.querySelectorAll('span');
-  const userIDs = Array.from(userSpans).map(span => span.dataset.user_id);
-
-  try {
-    const res = await fetch('/groups', {
-      method  : 'POST',
-      headers : { 'Content-Type' : 'application/json' },
-      body    : JSON.stringify({
-        name    : minigameForm.querySelector('groupName'),
-        friends : userIDs
-      })
-    });
-    const data = await res.json();
-
-    // Programmatically open the modal
-    let modalId;
-    switch (minigameSelect.value) {
-      case 'minigame1':
-        modalId = 'minigameModal1';
-        break;
-      case 'minigame2':
-        modalId = 'minigameModal2';
-        break;
-      case 'minigame3':
-        modalId = 'minigameModal3';
-        break;
-      default:
-        modalId = null;
-        break;
-    }
-
-    if (modalId) {
-      const myModal = new bootstrap.Modal(document.getElementById(modalId));
-      myModal.show();
-    }
-
-  } catch (err) {
-    console.error(err);
+  let gameFn;
+  switch (selection.value) {
+    case 'quickdraw':
+      gameFn    = quickdraw;
+      break;
   }
-});
 
-document.getElementById('minigameAddFriendBtn').addEventListener('click', async () => {
-  const selectedFriends = document.getElementById('minigameSelectedFriends');
-  const minigameForm = document.getElementById('minigameForm');
-  const friendID = minigameForm.querySelector('#minigameFriendSelect').value;
-
-  try {
-    const res = await fetch(`/users/${friendID}`, {
-      method : 'GET',
-      headers : { 'Content-Type' : 'application/json' }
-    });
-    const data = await res.json();
-
-    // Check if a span with the same username already exists
-    const existingFriendSpan = selectedFriends.querySelector(`span[data-username="${data.username}"]`);
-
-    if (!existingFriendSpan) {
-      const friendSpan = document.createElement('span');
-      const rmFriendBtn = document.createElement('button');
-
-      friendSpan.textContent = data.username;
-      friendSpan.dataset.username = data.username;
-      friendSpan.dataset.user_id = data.user_id;
-      friendSpan.classList = 'badge bg-primary d-flex align-items-center';
-
-      rmFriendBtn.classList = 'btn-close btn-close-white ms-2';
-      rmFriendBtn.addEventListener('click', () => {
-        selectedFriends.removeChild(friendSpan);
-      });
-
-      friendSpan.appendChild(rmFriendBtn);
-      selectedFriends.appendChild(friendSpan);
-    }
-
-  } catch (err) {
-    console.error(err);
+  if (gameFn) {
+    modal.hide();
+    gameFn();
+  } else {
+    console.error('Please select a game');
   }
-});
+})
+
+function quickdraw() {
+  console.log('playing quickdraw');
+}
