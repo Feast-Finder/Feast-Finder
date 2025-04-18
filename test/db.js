6e97119b75ec9ed82eaded98d6a70b162b45399d
Samuel Garmany <65299214+samuel-garmany@users.noreply.github.com>
2025-04-17 23:46:13 -0600

    Fix CI flow

--- test/db.js
+++ test/db.js
@@ -1,13 +1,13 @@
 // db.js
 
 const conf = {
-  db:       process.env.POSTGRES_DB,
-  user:     process.env.POSTGRES_USER,
+  db: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
+  host: 'db' 
 };
 
 const pgp = require('pg-promise')();
-require('dotenv').config();
-const connectionString =  `postgres://${conf.user}:${conf.password}@db:5432/${conf.db}`;
+const connectionString = `postgres://${conf.user}:${conf.password}@${conf.host}:5432/${conf.db}`;
 const db = pgp(connectionString);
 module.exports = db;
